94ea6407f24f9b85641c73552c5a460a5c49a29e
pattonw <pattonw@hhmi.org>
2023-08-26 15:32:14 -0400

    update view_validations impl

--- care/cli/v2/view_validations.py
+++ care/cli/v2/view_validations.py
@@ -6,9 +6,9 @@ import click
 @click.option("-i", "--iteration", type=int, default=0)
 @click.option("-c", "--crop", type=int)
 def view_validations(train_config, iteration, crop):
-    from care.configs import TrainConfig, DataConfig, DataSetConfig
+    from care.configs import TrainConfig, DataConfig, StorageConfig
 
-    import daisy
+    from funlib.persistence import open_ds
 
     import neuroglancer
     import numpy as np
@@ -26,6 +26,7 @@ def view_validations(train_config, iteration, crop):
     validation_pred_dataset = "volumes/val/{crop}/{i}/pred"
     validation_emb_dataset = "volumes/val/{crop}/{i}/emb"
     validation_raw_dataset = "volumes/val/{crop}/raw"
+    validation_care_dataset = "volumes/val/{crop}/{i}/care"
 
     datasets: list[tuple[list[str], list[str], list[str], str]] = []
 
@@ -33,34 +34,9 @@ def view_validations(train_config, iteration, crop):
         raw = f"{crop}_raw"
         try:
             dataset = raw
-            daisy_array = daisy.open_ds(
+            daisy_array = open_ds(
                 f"{train_config.validation_container}",
                 validation_raw_dataset.format(i=iteration, crop=crop),
-            )
-
-            dims = neuroglancer.CoordinateSpace(
-                names=["z", "y", "x"],
-                units="nm",
-                scales=daisy_array.voxel_size,
-            )
-
-            raw_vol = neuroglancer.LocalVolume(
-                data=daisy_array.data,
-                voxel_offset=(*(daisy_array.roi.offset / daisy_array.voxel_size),),
-                dimensions=dims,
-            )
-
-            s.layers[dataset] = neuroglancer.ImageLayer(source=raw_vol, opacity=1.0)
-        except KeyError:
-            pass
-
-        # pred
-        pred = f"{crop}_pred"
-        try:
-            dataset = pred
-            daisy_array = daisy.open_ds(
-                f"{train_config.validation_container}",
-                validation_pred_dataset.format(i=iteration, crop=crop),
             )
 
             dims = neuroglancer.CoordinateSpace(
@@ -70,87 +46,54 @@ def view_validations(train_config, iteration, crop):
             )
             voxel_offset = (
                 0,
-                *((daisy_array.roi.offset / daisy_array.voxel_size) + 13),
+                *((daisy_array.roi.offset / daisy_array.voxel_size)),
             )
 
-            pred_data = daisy_array.to_ndarray(daisy_array.roi)
-            sub_arrays = [
-                pred_data[i][tuple(slice(13 - a, (-13 - a) or None, 1) for a in n)]
-                for i, n in enumerate(data_config.neighborhood)
-            ]
-            care_data = np.stack(
-                sub_arrays,
-                axis=0,
-            )
-
-            pred_vol = neuroglancer.LocalVolume(
-                data=care_data,
+            raw_vol = neuroglancer.LocalVolume(
+                data=daisy_array.data,
                 voxel_offset=voxel_offset,
                 dimensions=dims,
             )
 
-            s.layers[dataset] = neuroglancer.ImageLayer(source=pred_vol, opacity=1.0)
-
-            # add debug layers:
-            dims2 = neuroglancer.CoordinateSpace(
-                names=["z", "y", "x"],
-                units="nm",
-                scales=(*daisy_array.voxel_size,),
-            )
-            voxel_offset2 = (
-                *((daisy_array.roi.offset / daisy_array.voxel_size) + 13),
-            )
-
-            means = care_data.mean(axis=0)
-            debug_vol = neuroglancer.LocalVolume(
-                data=means,
-                voxel_offset=voxel_offset2,
-                dimensions=dims2,
-            )
-
-            s.layers["mean"] = neuroglancer.ImageLayer(source=debug_vol, opacity=1.0)
-
-            variances = care_data.var(axis=0)
-            debug_vol = neuroglancer.LocalVolume(
-                data=variances,
-                voxel_offset=voxel_offset2,
-                dimensions=dims2,
-            )
-
-            s.layers["var"] = neuroglancer.ImageLayer(source=debug_vol, opacity=1.0)
-
-            normed = (care_data - means) / variances
-            debug_vol = neuroglancer.LocalVolume(
-                data=normed,
-                voxel_offset=voxel_offset,
-                dimensions=dims,
-            )
-
-            s.layers["normed"] = neuroglancer.ImageLayer(source=debug_vol, opacity=1.0)
-
-            debug_vol = neuroglancer.LocalVolume(
-                data=normed.mean(axis=0),
-                voxel_offset=voxel_offset2,
-                dimensions=dims2,
-            )
-
-            s.layers["normed2"] = neuroglancer.ImageLayer(source=debug_vol, opacity=1.0)
-
-            
-
+            s.layers[dataset] = neuroglancer.ImageLayer(source=raw_vol, opacity=1.0)
         except KeyError:
             pass
 
-        return (raw, pred, ["mean", "var"])
+
+        care = f"{crop}_care"
+        try:
+            dataset = care
+            daisy_array = open_ds(
+                f"{train_config.validation_container}",
+                validation_care_dataset.format(i=iteration, crop=crop),
+            )
+
+            dims = neuroglancer.CoordinateSpace(
+                names=["c^", "z", "y", "x"],
+                units="nm",
+                scales=(1, *daisy_array.voxel_size),
+            )
+            voxel_offset = (
+                0,
+                *((daisy_array.roi.offset / daisy_array.voxel_size)),
+            )
+
+            raw_vol = neuroglancer.LocalVolume(
+                data=daisy_array.data,
+                voxel_offset=voxel_offset,
+                dimensions=dims,
+            )
+
+            s.layers[dataset] = neuroglancer.ImageLayer(source=raw_vol, opacity=1.0)
+        except KeyError:
+            pass
+
+        return (raw, care)
 
     with viewer.txn() as s:
         for dataset_yaml in data_config.datasets:
-            dataset_config = DataSetConfig(**yaml.safe_load(dataset_yaml.open().read()))
-            for validation_crop in dataset_config.validation:
-                if validation_crop == crop:
-                    print(f"Adding layers for {validation_crop}")
-                    raw, pred, debug = add_layers(s, validation_crop)
-                    datasets.append((raw, pred, debug))
+            raw, pred = add_layers(s, dataset_yaml.name[:-5])
+            datasets.append((raw, pred))
 
         s.layout = neuroglancer.row_layout(
             [
@@ -165,13 +108,6 @@ def view_validations(train_config, iteration, crop):
                     [
                         neuroglancer.LayerGroupViewer(
                             layers=[dataset[1] for dataset in datasets]
-                        ),
-                    ]
-                ),
-                neuroglancer.column_layout(
-                    [
-                        neuroglancer.LayerGroupViewer(
-                            layers=list(itertools.chain([dataset[2] for dataset in datasets]))
                         ),
                     ]
                 ),
