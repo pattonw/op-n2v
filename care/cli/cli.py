1572301adf9b8a2022ef9c50412161d7e649f8d2
pattonw <pattonw@hhmi.org>
2023-08-26 15:30:21 -0400

    remove v1

--- care/cli/cli.py
+++ care/cli/cli.py
@@ -3,7 +3,6 @@ import yaml
 
 import sys
 
-from .v1 import v1
 from .v2 import v2
 
 
@@ -12,7 +11,6 @@ def main(args=None):
     """Console script for care."""
     return None
 
-main.add_command(v1)
 main.add_command(v2)
 
 if __name__ == "__main__":
