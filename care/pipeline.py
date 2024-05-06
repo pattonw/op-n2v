d97f9c8d036a41d6bca631f79d7503a45ca4e8e4
William Patton <wllmpttn24@gmail.com>
2024-05-06 14:58:48 +0200

    simplify pipeline

--- care/pipeline.py
+++ care/pipeline.py
@@ -2,6 +2,7 @@ from .configs import DataConfig
 from .gp.scale_provider import ScaleProvider
 from .gp.context import Context
 from .gp.histogram_equalization import HistogramEqualization
+from .gp.tiff_stack_source import TiffStackSource
 
 import gunpowder as gp
 import daisy
@@ -25,55 +26,22 @@ def build_pipeline(
     raw_input_key = gp.ArrayKey("RAW_INPUT")
     raw_context_key = gp.ArrayKey("RAW_CONTEXT")
 
-    resolution_pipelines = defaultdict(lambda: list())
-    for dataset_yaml in data_config.datasets:
+    dataset_pipelines = []
+    for dataset_path in data_config.datasets:
 
-        dataset_info = yaml.safe_load(dataset_yaml.open().read())
-
-        raw_info = dataset_info
-        dataset_container: str = raw_info.get("container", RAW_CONTAINER).format(
-            dataset=dataset_yaml.name[:-5]
+        pipeline = (
+            TiffStackSource(raw_input_key, dataset_path)
+            # + gp.Normalize(raw_input_key)
+            + gp.IntensityAugment(raw_input_key, 0.9, 1.1, -0.1, 0.1, clip=False)
+            + Context(raw_input_key, raw_context_key, data_config.neighborhood)
+            + HistogramEqualization(raw_context_key, raw_context_key)
+            + gp.RandomLocation()
         )
-        raw_dataset_name: str = raw_info.get("dataset", RAW_DATASET)
-        has_scale_levels = "scale_level" in [
-            tup[1]
-            for tup in string.Formatter().parse(raw_dataset_name)
-            if tup[1] is not None
-        ]
+        dataset_pipelines.append(pipeline)
 
-        for scale_level in range(5):
+    pipeline = tuple(dataset_pipelines) + gp.RandomProvider()
 
-            if not has_scale_levels and scale_level > 0:
-                continue
-
-            # get raw container
-            raw_dataset_level = raw_dataset_name.format(level=scale_level)
-            raw_dataset: Array = open_ds(
-                f"{dataset_container}",
-                raw_dataset_level,
-            )
-
-            pipeline = (
-                gp.ZarrSource(
-                    str(dataset_container),
-                    {raw_input_key: raw_dataset_level},
-                    {raw_input_key: gp.ArraySpec(voxel_size=raw_dataset.voxel_size)},
-                )
-                + gp.Normalize(raw_input_key)
-                + gp.IntensityAugment(raw_input_key, 0.9, 1.1, -0.1, 0.1)
-                + Context(raw_input_key, raw_context_key, data_config.neighborhood)
-                + HistogramEqualization(raw_context_key, raw_context_key)
-                + gp.RandomLocation()
-            )
-            resolution_pipelines[raw_dataset.voxel_size].append(pipeline)
-
-    scale_pipelines = tuple(
-        tuple(dataset_pipelines) + gp.RandomProvider()
-        for _, dataset_pipelines in resolution_pipelines.items()
-    )
-    pipeline = tuple(scale_pipelines) + ScaleProvider(scale_key=raw_input_key)
-
-    return pipeline, list(resolution_pipelines.keys())
+    return pipeline
 
 
 def get_request(shape: Coordinate, out_shape) -> gp.BatchRequest:
